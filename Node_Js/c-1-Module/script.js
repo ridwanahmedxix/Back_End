f247613c002ea35957c590ab40e72faa9c3adc0d
ridwancodexr <ridwancodexr@gmail.com>
2026-01-03 14:19:00 +0600

    Module | Local module - exports, require

---
+++ Node_Js/c-1-Module/script.js
@@ -0,0 +1,4 @@
+const s1 = require("./student");
+console.log(s1.getNmae());
+console.log(s1.getAge());
+console.log(s1.GPA);
---
+++ Node_Js/c-1-Module/student.js
@@ -0,0 +1,19 @@
+const getNmae = () => {
+  return "Ridwan Ahmed";
+};
+
+const getAge = () => {
+  return "18";
+};
+
+const GPA = 3.77;
+
+// exports.getNmae = getNmae;
+// exports.getAge = getAge;
+// exports.myGPA = GPA;
+
+module.exports = {
+  getNmae,
+  getAge,
+  GPA,
+};
