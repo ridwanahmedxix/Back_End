f902b50d183ea6ef7ee1d9cd18784e0cae137cdb
ridwancodexr <ridwancodexr@gmail.com>
2026-01-03 18:37:20 +0600

    all code clear

--- NodeJs/script.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-fs.rename("demo1.txt", "demoFile.txt", (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("successful");
-    // console.log(data);
-  }
-});
