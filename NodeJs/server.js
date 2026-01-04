c504c3aeae15bc92ebdcfcc626489efac0f07cca
ridwancodexr <ridwancodexr@gmail.com>
2026-01-04 12:45:49 +0600

    server create revision 1

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -1,11 +1,22 @@
+// const http = require("http");
+// const port = 3000;
+// const hostNmae = "127.0.0.1";
+
+// const myServer = http.createServer((req, res) => {
+//   res.end("Hello , I am your first server");
+// });
+
+// myServer.listen(port, hostNmae, () => {
+//   console.log(`our server is runing successful at http://${hostNmae}:${port} `);
+// });
+
 const http = require("http");
 const port = 3000;
 const hostNmae = "127.0.0.1";
 
 const myServer = http.createServer((req, res) => {
-  res.end("Hello , I am your first server");
+  res.end("Hello , This is my First Server ");
 });
-
 myServer.listen(port, hostNmae, () => {
-  console.log(`our server is runing successful at http://${hostNmae}:${port} `);
+  console.log(`The server is running at http://${hostNmae}:${port}`);
 });
