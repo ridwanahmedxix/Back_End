049daa5cceca903e3bc89a37a78e9996ac1b1907
ridwancodexr <ridwancodexr@gmail.com>
2026-01-05 12:43:08 +0600

    git status & server create revision

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -3,3 +3,17 @@
 
 // const randomSong = require("song-object-generator");
 // console.log(randomSong());
+
+const http = require("http");
+const port = 3000;
+const hostNmae = "127.0.0.1";
+
+const myServer = http.createServer((req, res) => {
+  res.writeHead(202, { "Content-Type": "text/plain" });
+  res.write("Hello , this is my first server");
+  res.end();
+});
+
+myServer.listen(port, hostNmae, () => {
+  console.log(`The server is runing at http://${hostNmae}:${port}`);
+});
