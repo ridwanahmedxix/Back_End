3d79ee6212fcf32e08a9e92dd32c5ebd75722b0a
ridwancodexr <ridwancodexr@gmail.com>
2026-01-05 10:11:23 +0600

    code status revision

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -1,25 +1,15 @@
-// const http = require("http");
-// const port = 3000;
-// const hostName = "127.0.0.1";
-
-// const myServer = http.createServer((req, res) => {
-//   res.writeHead(202, { "Content-Type": "text/plain" });
-//   res.write("Hello , bro this is my first server");
-//   res.end();
-// });
-// myServer.listen(port, hostName, () => {
-//   console.log(`The server is runing at http://${hostName}:${port}`);
-// });
-
 const http = require("http");
 const port = 3000;
 const hostNmae = "127.0.0.1";
 
 const myServer = http.createServer((req, res) => {
-  res.writeHead(202, { "Content-Type": "text/plain" });
-  res.write("Hello everyone , this is my first server");
+  res.writeHead(202, { "Content-Type": "text-html" });
+  res.write(" <h1> Hello Everyone </h1> ");
+  res.write(" <h1> This is Ridwan  </h1> ");
+  res.write(" <h1> And I am a Full stack Web developer </h1> ");
   res.end();
 });
+
 myServer.listen(port, hostNmae, () => {
-  console.log(`The server is runing at http://${hostNmae}:${port}`);
+  console.log(`The server is Runing at http://${hostNmae}:${port}`);
 });
