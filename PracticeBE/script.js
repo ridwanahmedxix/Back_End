386c5e3600a258c2271b74ea055efe16378e8700
ridwancodexr <ridwancodexr@gmail.com>
2026-01-05 21:07:27 +0600

    routing- nodemon revision - 4

--- PracticeBE/script.js
+++ PracticeBE/script.js
@@ -5,25 +5,25 @@ const hostName = "127.0.0.1";
 const myServer = http.createServer((req, res) => {
   if (req.url === "/") {
     fs.readFile("index.html", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
+      res.writeHead(200, { Content_Type: "text/html" });
       res.write(data);
       res.end();
     });
   } else if (req.url === "/about") {
     fs.readFile("about.html", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
+      res.writeHead(200, { Content_Type: "text/html" });
       res.write(data);
       res.end();
     });
   } else if (req.url === "/contact") {
     fs.readFile("contact.html", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
+      res.writeHead(200, { Content_Type: "text/html" });
       res.write(data);
       res.end();
     });
   } else {
     fs.readFile("error.html", (err, data) => {
-      res.writeHead(404, { "Content-Type": "text/html" });
+      res.writeHead(200, { Content_Type: "text/html" });
       res.write(data);
       res.end();
     });
@@ -31,7 +31,5 @@ const myServer = http.createServer((req, res) => {
 });
 
 myServer.listen(port, hostName, () => {
-  console.log(`The Server Is Runing At http://${hostName}:${port}`);
+  console.log(`The Server Is Running At http://${hostName}:${port}`);
 });
-
-// Revision Number 3
