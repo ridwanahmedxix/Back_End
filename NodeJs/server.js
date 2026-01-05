7171c47e2e110a065ff26c08672da918c9a84005
ridwancodexr <ridwancodexr@gmail.com>
2026-01-05 08:17:21 +0600

    First Commit

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -8,3 +8,15 @@
 // myServer.listen(port, hostNmae, () => {
 //   console.log(`The server is running at http://${hostNmae}:${port}`);
 // });
+
+const http = require("http");
+const port = 3000;
+const hostName = "120.0.0.1";
+
+const myServer = http.createServer((req, res) => {
+  res.end("Hello , This is my First Server");
+});
+
+myServer.listen(port, hostName, () => {
+  console.log(`The Server is running at http://${hostName}:${port}`);
+});
