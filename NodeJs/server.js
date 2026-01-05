76c2d45950efd0009b3e5fc0fb11d48efd8e8311
ridwancodexr <ridwancodexr@gmail.com>
2026-01-05 18:21:23 +0600

    Server create & Code Status

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -1,19 +1,12 @@
-// const randomFruitsName = require("random-fruits-name");
-// console.log(randomFruitsName("en"));
-
-// const randomSong = require("song-object-generator");
-// console.log(randomSong());
-
 const http = require("http");
 const port = 3000;
-const hostNmae = "127.0.0.1";
+const hostName = "127.0.0.1";
 
 const myServer = http.createServer((req, res) => {
   res.writeHead(202, { "Content-Type": "text/plain" });
   res.write("Hello , this is my first server");
   res.end();
 });
-
-myServer.listen(port, hostNmae, () => {
-  console.log(`The server is runing at http://${hostNmae}:${port}`);
+myServer.listen(port, hostName, () => {
+  console.log(`The server is runing at http://${hostName}:${port}`);
 });
