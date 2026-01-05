4b85afab1e9227eb600b4c5ad1b1175624e6f929
ridwancodexr <ridwancodexr@gmail.com>
2026-01-05 09:11:49 +0600

    code status - 2

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -4,7 +4,6 @@
 
 // const myServer = http.createServer((req, res) => {
 //   res.writeHead(202, { "Content-Type": "text/plain" });
-//   res.writeHead(202, { ContentType: "text/html" });
 //   res.write("Hello , bro this is my first server");
 //   res.end();
 // });
@@ -18,10 +17,10 @@ const hostName = "127.0.0.1";
 
 const myServer = http.createServer((req, res) => {
   res.writeHead(202, { "Content-Type": "text/plain" });
-  res.write("Hello this is my first server");
+  res.write("Hello Bruh");
   res.end();
 });
 
 myServer.listen(port, hostName, () => {
-  console.log(`The server is runing at http://${hostName}:${port}`);
+  console.log(`Runing at http://${hostName}:${port}`);
 });
