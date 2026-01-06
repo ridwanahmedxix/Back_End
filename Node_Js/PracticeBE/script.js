dc74d62daaaf4ae6f97f920c1c3eb9406d14b956
ridwancodexr <ridwancodexr@gmail.com>
2026-01-06 18:40:00 +0600

    express install and practice setup

--- Node_Js/PracticeBE/script.js
+++ Node_Js/PracticeBE/script.js
@@ -1,35 +1,6 @@
-const http = require("http");
-const fs = require("fs");
+const app = require("./app");
 const port = 3000;
-const hostName = "127.0.0.1";
-const myServer = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile("index.html", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
-  } else if (req.url === "/about") {
-    fs.readFile("about.html", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
-  } else if (req.url === "/contact") {
-    fs.readFile("contact.html", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
-  } else {
-    fs.readFile("error.html", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
-  }
-});
 
-myServer.listen(port, hostName, () => {
-  console.log(`The Server Is Runing At http://${hostName}:${port}`);
+app.listen(port, () => {
+  console.log(`The server is runing at http://localhost:${port}`);
 });
