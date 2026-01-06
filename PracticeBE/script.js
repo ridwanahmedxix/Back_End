0e78b4568d374bb17009e04ff084c2a23ce15123
ridwancodexr <ridwancodexr@gmail.com>
2026-01-06 08:12:38 +0600

    routing- nodemon today  revision

---
+++ PracticeBE/script.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const fs = require("fs");
+const port = 3000;
+const hostName = "127.0.0.1";
+const myServer = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile("index.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url === "/about") {
+    fs.readFile("about.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url === "/contact") {
+    fs.readFile("contact.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else {
+    fs.readFile("error.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  }
+});
+myServer.listen(port, hostName, () => {
+  console.log(`The server is runing at http://${hostName}:${port}`);
+});
