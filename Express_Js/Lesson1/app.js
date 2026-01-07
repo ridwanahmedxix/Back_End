036b746ba849353fbf7c3c41349be59c2607bab8
ridwancodexr <ridwancodexr@gmail.com>
2026-01-07 10:30:41 +0600

    get/post/put/delete

--- Express_Js/Lesson1/app.js
+++ Express_Js/Lesson1/app.js
@@ -2,15 +2,22 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(" I am a get request at home route");
+  res.send(" |get| I am a get request at home route");
+  res.end();
 });
 
-app.get("/about", (req, res) => {
-  res.send(" This is ABOUT Section");
+app.post("/", (req, res) => {
+  res.send(" |post|  How are you ? I Hope you are fine .");
+  res.end();
 });
 
-app.get("/ridwan", (req, res) => {
-  res.send(" Itz Ridwan , What's up !?");
+app.put("/", (req, res) => {
+  res.send(" |put| This is a Put mama  ");
+  res.end();
+});
+app.delete("/", (req, res) => {
+  res.send(" |Delete| This is - Delete kor taratari  ");
+  res.end();
 });
 
 module.exports = app;
