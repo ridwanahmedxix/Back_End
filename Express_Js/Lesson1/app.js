96b9bb58b740a477a76b5d96c3e1204732974926
ridwancodexr <ridwancodexr@gmail.com>
2026-01-07 10:47:54 +0600

    html file create

--- Express_Js/Lesson1/app.js
+++ Express_Js/Lesson1/app.js
@@ -1,23 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
+app.get("/contact", (req, res) => {
+  res.send("my email address - example@gmail.com");
+  res.end();
+});
+
 app.get("/", (req, res) => {
-  res.send(" |get| I am a get request at home route");
-  res.end();
-});
-
-app.post("/", (req, res) => {
-  res.send(" |post|  How are you ? I Hope you are fine .");
-  res.end();
-});
-
-app.put("/", (req, res) => {
-  res.send(" |put| This is a Put mama  ");
-  res.end();
-});
-app.delete("/", (req, res) => {
-  res.send(" |Delete| This is - Delete kor taratari  ");
-  res.end();
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 module.exports = app;
