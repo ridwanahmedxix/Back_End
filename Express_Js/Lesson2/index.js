e070db2a77e7d63525f8466739a796f1f1115c16
ridwancodexr <ridwancodexr@gmail.com>
2026-01-08 11:29:02 +0600

    http request with route parameter and header

--- Express_Js/Lesson2/index.js
+++ Express_Js/Lesson2/index.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 3004;
 
 app.get("/", (req, res) => {
-  const { id, name } = req.query;
-  res.send(`Student Name Is : ${name} , And Id Is : ${id}`);
+  const id = req.header("id");
+  const name = req.header("name");
+
+  res.send(`The Id Is : ${id} , The Nmae Is : ${name}`);
 });
 
 app.listen(port, () => {
