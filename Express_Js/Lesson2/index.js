c824f407ac51b05bc27637e6eeee4994a4bf506c
ridwancodexr <ridwancodexr@gmail.com>
2026-01-08 14:46:18 +0600

    http request with route parameter and header revision

--- Express_Js/Lesson2/index.js
+++ Express_Js/Lesson2/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
-const port = 3004;
+const port = 3009;
 
 app.get("/", (req, res) => {
   const id = req.header("id");
   const name = req.header("name");
 
-  res.send(`The Id Is : ${id} , The Nmae Is : ${name}`);
+  res.send(`The id is : ${id} and nami is : ${name}`);
 });
 
 app.listen(port, () => {
-  console.log(`The Server is runing at http://localhost:${port}`);
+  console.log(`The server is runing at http://localhost:${port}`);
 });
