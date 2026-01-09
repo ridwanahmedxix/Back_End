6686f1b9998eb9cdcc16575e49fd4d605202e925
ridwancodexr <ridwancodexr@gmail.com>
2026-01-09 08:36:42 +0600

    First commit today

--- Express_Js/Lesson2/index.js
+++ Express_Js/Lesson2/index.js
@@ -1,16 +1,1 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const port = 3000;
-
-app.use(bodyParser.json());
-
-app.post("/user", (req, res) => {
-  const name = req.body.name;
-  const age = req.body.age;
-  res.send(` Welcome ${name} and your Age ${age} `);
-});
-
-app.listen(port, () => {
-  console.log(`The server is runing at http://localhost:${port}`);
-});
