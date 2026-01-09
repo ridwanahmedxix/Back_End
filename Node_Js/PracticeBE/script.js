dc7d33a4f9c0561f2c55f0d5946f69fdf95be7b7
ridwancodexr <ridwancodexr@gmail.com>
2026-01-09 18:16:18 +0600

    last class all code revise

--- Node_Js/PracticeBE/script.js
+++ Node_Js/PracticeBE/script.js
@@ -1,6 +1,19 @@
-const app = require("./app");
-const port = 3000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3002;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/registar", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.post("/registar", (req, res) => {
+  const { fullName, email } = req.body;
+  res.send(`Welcome ${fullName} and Your Email ${email} `);
+});
 
 app.listen(port, () => {
-  console.log(`The server is runing at http://localhost:${port}`);
+  console.log(`The Server Is Runing At http://localhost:${port}`);
 });
