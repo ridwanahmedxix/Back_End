aec1172df02c8f82ce9c5cd640d579a7c8c4fd01
ridwancodexr <ridwancodexr@gmail.com>
2026-01-09 08:58:27 +0600

    Body Parser Revision

--- Express_Js/Lesson2/index.js
+++ Express_Js/Lesson2/index.js
@@ -1,1 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3003;
+const hostName = "127.0.0.1";
+
+app.use(bodyParser.json());
+
+app.post("/user", (req, res) => {
+  const { name, id, age } = req.body;
+  res.send(`The name : ${name} and the id ${id} and the age ${age} `);
+});
+
+app.listen(port, hostName, () => {
+  console.log(`The server is runing at http://${hostName}:${port}`);
+});
