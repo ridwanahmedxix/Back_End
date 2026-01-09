3a56753b67dc7198ecf0d2afb31229c63c2b2409
ridwancodexr <ridwancodexr@gmail.com>
2026-01-09 18:50:20 +0600

    Area Calculator project Done

--- Projects/Pro-2-Area Calculator project/index.js
+++ Projects/Pro-2-Area Calculator project/index.js
@@ -3,8 +3,34 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3008;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/circle", (req, res) => {
+  res.sendFile(__dirname + "/circle.html");
+});
+
+app.post("/circle", (req, res) => {
+  const radius = req.body.radius;
+
+  const area = Math.PI * radius * radius;
+  res.send(` <h2> Area Of Circle : ${area}  </h2>  `);
+});
+
+app.get("/triangle", (req, res) => {
+  res.sendFile(__dirname + "/triangle.html");
+});
+
+app.post("/triangle", (req, res) => {
+  const base = req.body.base;
+  const height = req.body.height;
+
+  const area = 0.5 * base * height;
+  res.send(` <h2> Area Of Triangle : ${area}  </h2>  `);
 });
 
 app.listen(port, () => {
