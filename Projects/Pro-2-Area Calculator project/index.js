46b62b96615ca11cd42cc4919fb630d268587703
ridwancodexr <ridwancodexr@gmail.com>
2026-01-09 10:32:22 +0600

    Area Calculator project Basic Setup

--- Projects/Pro-2-Area Calculator project/index.js
+++ Projects/Pro-2-Area Calculator project/index.js
@@ -3,6 +3,10 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3008;
 
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
 app.listen(port, () => {
   console.log(`The Server Is Runing At http://localhost:${port}`);
 });
