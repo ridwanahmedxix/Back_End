791f5a5e4a789f0109f99fee97925d45513d074f
ridwancodexr <ridwancodexr@gmail.com>
2026-01-10 18:53:32 +0600

    server create

--- Express_Js/Lesson5/index.js
+++ Express_Js/Lesson5/index.js
@@ -1,3 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3008;
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log(`The server is runing at http://localhost:${port}`);
+});
