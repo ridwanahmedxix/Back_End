766def0faad8a7e2dae2065af042ff9c0465263b
ridwancodexr <ridwancodexr@gmail.com>
2026-01-10 18:33:36 +0600

    How to set up environment variables

--- Express_Js/Lesson4/index.js
+++ Express_Js/Lesson4/index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-const port = 3004;
+const port = process.env.port || 3000;
 
 app.get("/", (req, res) => {
   res.send("hello");
