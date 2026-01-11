a17c5e04702e60230c8aaa4e29911767f05ae2e2
ridwancodexr <ridwancodexr@gmail.com>
2026-01-11 19:13:05 +0600

    Route Create

--- Practice/index.js
+++ Practice/index.js
@@ -6,6 +6,16 @@ const port = 3005;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/circle", (req, res) => {
+  res.sendFile(__dirname + "/circle.html");
+});
+app.get("/triangle", (req, res) => {
+  res.sendFile(__dirname + "/triangle.html");
+});
+
 app.listen(port, () => {
   console.log(`The Server Is Runing At http://localhost:${port}`);
 });
