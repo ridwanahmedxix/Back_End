31c092997639fa1858934aa7e9e06fc6bd2435cd
ridwancodexr <ridwancodexr@gmail.com>
2026-01-11 19:06:41 +0600

    Some Update

--- Practice/index.js
+++ Practice/index.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3005;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.listen(port, () => {
   console.log(`The Server Is Runing At http://localhost:${port}`);
 });
