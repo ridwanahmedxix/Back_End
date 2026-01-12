24ea77815138f364a51172de63b36cb37781be1b
ridwancodexr <ridwancodexr@gmail.com>
2026-01-12 20:00:32 +0600

    Last Class Code Revision done

--- Practice/p-2/index.js
+++ Practice/p-2/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3005;
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
 app.listen(port, () => {
   console.log(`The server is runing at http://localhost:${port}`);
 });
