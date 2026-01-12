d8992b8d47b15afe54aff908004a78fe65848e94
ridwancodexr <ridwancodexr@gmail.com>
2026-01-12 21:55:41 +0600

    Model View Controller File Create

--- Express_Js/MVC_Architecture/index.js
+++ Express_Js/MVC_Architecture/index.js
@@ -1,52 +1,9 @@
 const express = require("express");
+const userRouter = require("./routes/userRoutes");
 const app = express();
 const port = 3005;
+app.use(userRouter);
 app.use(express.urlencoded({ extended: true }));
-
-const users = [
-  {
-    name: "Ridwan Ahmed",
-    age: 18,
-  },
-
-  {
-    name: "Jannatin Naim",
-    age: 22,
-  },
-  {
-    name: "Aideed",
-    age: 23,
-  },
-  {
-    name: "Nusrat Jahan",
-    age: 15,
-  },
-];
-
-const htmlForm = ` 
-    <form action="/user" method="post" >
-      <input type="text" name="name" placeholder="Enter Name" />
-      <input type="number" name="age" placeholder="Enter Age" />
-      <button type="submit">Save User</button>
-    </form>
-`;
-
-app.get("/user", (req, res) => {
-  res.send(htmlForm);
-});
-app.post("/user", (req, res) => {
-  const name = req.body.name;
-  const age = req.body.age;
-  const user = {
-    name,
-    age,
-  };
-  users.push(user);
-  res.status(201).json({
-    success: true,
-    users,
-  });
-});
 
 app.use((req, res, next) => {
   res.status(404).json({
---
+++ Express_Js/MVC_Architecture/models/userModel.js
@@ -0,0 +1,21 @@
+const users = [
+  {
+    name: "Ridwan Ahmed",
+    age: 18,
+  },
+
+  {
+    name: "Jannatin Naim",
+    age: 22,
+  },
+  {
+    name: "Aideed",
+    age: 23,
+  },
+  {
+    name: "Nusrat Jahan",
+    age: 15,
+  },
+];
+
+module.exports = users;
