3ee1e7ceba4332eba73550ad37480ef3b128fe81
ridwancodexr <ridwancodexr@gmail.com>
2026-01-13 08:29:29 +0600

    New MVC architecture create done

--- Express_Js/MVC_Architecture/index.js
+++ Express_Js/MVC_Architecture/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const userRouter = require("./routes/userRoutes");
+const productRouter = require("./routes/productsRoutes");
+
 const app = express();
 const port = 3005;
 
@@ -9,6 +11,7 @@ app.use(express.json());
 
 // routes
 app.use(userRouter);
+app.use(productRouter);
 
 // 404 handler
 app.use((req, res, next) => {
