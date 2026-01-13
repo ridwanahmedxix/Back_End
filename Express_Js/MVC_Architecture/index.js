ca95b89fdc32cec150382c084b5779ce9ec84e8a
ridwancodexr <ridwancodexr@gmail.com>
2026-01-13 08:03:54 +0600

    TypeError Bug Fixed

--- Express_Js/MVC_Architecture/index.js
+++ Express_Js/MVC_Architecture/index.js
@@ -2,12 +2,18 @@ const express = require("express");
 const userRouter = require("./routes/userRoutes");
 const app = express();
 const port = 3005;
-app.use(userRouter);
-app.use(express.urlencoded({ extended: true }));
 
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// routes
+app.use(userRouter);
+
+// 404 handler
 app.use((req, res, next) => {
   res.status(404).json({
-    message: ` <h2> Wrong URL </h2>  `,
+    message: "Wrong URL",
   });
 });
 
