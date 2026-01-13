172fcca0791eed70341aec9e48f9f55533918531
ridwancodexr <ridwancodexr@gmail.com>
2026-01-13 08:34:13 +0600

    Products MVC architecture create

--- Express_Js/MVC_Architecture/controllers/productsController.js
+++ Express_Js/MVC_Architecture/controllers/productsController.js
@@ -3,7 +3,7 @@ const path = require("path");
 const products = require("../models/productsModel");
 
 exports.getProducts = (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "views", "product.html"));
 };
 
 exports.saveProducts = (req, res) => {
