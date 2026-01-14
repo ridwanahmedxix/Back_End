72eccacb70f6c90a88d7f7608c4b0b4b404a1f3b
ridwancodexr <ridwancodexr@gmail.com>
2026-01-14 09:16:41 +0600

    Env And Procfile Setup

--- REST-API/Lesson-1/index.js
+++ REST-API/Lesson-1/index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 const app = require("./app");
 
-const port = 4500;
+const port = process.env.port || 5000;
 app.listen(port, () => {
   console.log(`The Server Is Runing At http://localhost:${port}`);
 });
