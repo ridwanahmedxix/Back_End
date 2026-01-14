d2ef7e59be2dd812e88c28b215591741e8957f28
ridwancodexr <ridwancodexr@gmail.com>
2026-01-14 19:33:35 +0600

    Home page Create

--- REST-API-MongoDB/Lesson1/index.js
+++ REST-API-MongoDB/Lesson1/index.js
@@ -4,6 +4,7 @@ const port = 5000;
 const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // যদি JSON data পাঠাও
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/views/index.html"));
@@ -11,12 +12,23 @@ app.get("/", (req, res) => {
 app.post("/home", (req, res) => {
   const name = req.body.name;
   const password = req.body.password;
-  console.log(`Your Name Is : ${name} And Your Password : ${password} `);
+  // console.log(`Your Name Is : ${name} And Your Password : ${password} `);
   res.sendFile(path.join(__dirname + "/views/home.html"));
 });
-// app.post("/home", (req, res) => {
-//   res.sendFile(path.join(__dirname + "/views/home.html"));
-// });
+
+app.post("/info", (req, res) => {
+  const name = req.body.name;
+  const password = req.body.password;
+  res.send(`
+    
+    <h1> Your Name : ${name}  . </h1>  <br>
+    <h1> Your Password : ${password}  . </h1>  
+     
+    <form action="/home" method="post">
+      <button type="submit">Next</button>
+    </form>
+    `);
+});
 
 app.listen(port, () => {
   console.log(`The Server Is Runing At http://localhost:${port}`);
