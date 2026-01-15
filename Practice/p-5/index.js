6f1d444a5ded4986eac85610a83279e1da4dc368
ridwancodexr <ridwancodexr@gmail.com>
2026-01-15 20:41:56 +0600

    all update

--- Practice/p-5/index.js
+++ Practice/p-5/index.js
@@ -3,6 +3,10 @@ const multer = require("multer");
 const app = express();
 const PORT = 6000;
 
+app.get("/home", (req, res) => {
+  res.send("Welcome To Home Page");
+});
+
 app.listen(PORT, () => {
   console.log(`THE SERVER IS RUNING AT http://localhost:${PORT}`);
 });
