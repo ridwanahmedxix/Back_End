91ade7a9de8ac79a04974387a664c8fd03f084cc
ridwancodexr <ridwancodexr@gmail.com>
2026-01-15 11:39:02 +0600

    All update

--- REST-API-MongoDB/Lesson2/app.js
+++ REST-API-MongoDB/Lesson2/app.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 });
 app.use((err, req, res, next) => {
   res.status(500).json({
-    message: "The Server Is Not Found",
+    message: "The Server Is Not Foundj",
   });
 });
 
