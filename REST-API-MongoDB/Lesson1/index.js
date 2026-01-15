67dbea9df47c5b260f74af4231d54d019547c96b
ridwancodexr <ridwancodexr@gmail.com>
2026-01-15 11:49:13 +0600

    all update

--- REST-API-MongoDB/Lesson1/index.js
+++ REST-API-MongoDB/Lesson1/index.js
@@ -4,14 +4,14 @@ const port = 5000;
 const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); // যদি JSON data পাঠাও
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/views/index.html"));
 });
 app.post("/home", (req, res) => {
-  const name = req.body.name;
-  const password = req.body.password;
+  // const name = req.body.name;
+  // const password = req.body.password;
   // console.log(`Your Name Is : ${name} And Your Password : ${password} `);
   res.sendFile(path.join(__dirname + "/views/home.html"));
 });
@@ -27,6 +27,9 @@ app.post("/info", (req, res) => {
     <form action="/home" method="post">
       <button type="submit">Next</button>
     </form>
+
+
+
     `);
 });
 
