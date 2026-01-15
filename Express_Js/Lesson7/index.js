b4d66c49aa9c7130835bbdc23e72077704b24f60
ridwancodexr <ridwancodexr@gmail.com>
2026-01-15 19:43:46 +0600

    Upload files, images,

--- Express_Js/Lesson7/index.js
+++ Express_Js/Lesson7/index.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const multer = require("multer");
 const app = express();
 const PORT = 5500;
+
+// Multer - File Upload
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const name = Date.now() + "-" + file.originalname;
+    cb(null, name);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 app.get("/registar", (req, res) => {
   res.status(200).sendFile(__dirname + "/index.html");
 });
+
+app.post("/registar", upload.single("image"), (req, res) => {
+  res.status(200).send("File Is Uploaded");
+});
+
 app.get("/test", (req, res) => {
   res.status(200).send(" This a Test Api ");
 });
