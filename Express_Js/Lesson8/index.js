513ee681353c94b0fe363c3ab15676983e083c9a
ridwancodexr <ridwancodexr@gmail.com>
2026-01-16 19:43:00 +0600

    all update

--- Express_Js/Lesson8/index.js
+++ Express_Js/Lesson8/index.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const multer = require("multer");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 5500;
+
+// Conection To DB
+
+const conectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/userTestDB");
+    console.log(" DB Is Connected");
+  } catch (error) {
+    console.log(" DB Is Not Connected");
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 // Multer - File Upload
 
@@ -29,6 +43,7 @@ app.get("/test", (req, res) => {
   res.status(200).send(" This a Test Api ");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`THE SERVER IS RUNING AT http://localhost:${PORT}`);
+  await conectDB();
 });
