ff4c1eb32f4f1291fe8e77d6f90889126e7388c3
ridwancodexr <ridwancodexr@gmail.com>
2026-01-16 09:16:38 +0600

    server create

--- Practice/p-5/index.js
+++ Practice/p-5/index.js
@@ -1,33 +1,12 @@
 const express = require("express");
 const multer = require("multer");
 const app = express();
-const PORT = 4000;
-
-//Multer Image Upload Code -
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads");
-  },
-  filename: function (req, file, cb) {
-    const name = Date.now() + "-" + file.originalname;
-    cb(null, name);
-  },
-});
-
-const upload = multer({ storage: storage });
-
-app.get("/registar", (req, res) => {
-  res.status(200).sendFile(__dirname + "/index.html");
-});
-app.post("/registar", upload.single("image"), (req, res) => {
-  res.status(200).send("Image Is Uploaded");
-});
+const PORT = 4500;
 
 app.get("/home", (req, res) => {
-  res.status(200).send("Welcome To Home Page");
+  res.status(200).send("Welcome To The Home Page");
 });
 
 app.listen(PORT, () => {
-  console.log(`THE SERVER IS RUNING AT http://localhost:${PORT}`);
+  console.log(`The server is runing at http://localhost:${PORT}`);
 });
