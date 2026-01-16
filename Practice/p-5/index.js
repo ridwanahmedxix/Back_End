586ee87c6931e6e943b818153221eb10ef736615
ridwancodexr <ridwancodexr@gmail.com>
2026-01-16 18:53:07 +0600

    Image Upload Metod Create Successful

--- Practice/p-5/index.js
+++ Practice/p-5/index.js
@@ -3,6 +3,27 @@ const multer = require("multer");
 const app = express();
 const PORT = 5500;
 
+// Multer - File Upload
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const name = Date.now() + "-" + file.originalname;
+    cb(null, name);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.get("/registar", (req, res) => {
+  res.status(200).sendFile(__dirname + "/index.html");
+});
+app.post("/registar", upload.single("image"), (req, res) => {
+  res.status(200).send("Image Is Uploaded");
+});
+
 app.get("/home", (req, res) => {
   res.status(200).send(" Welcome To The Home Pag ");
 });
