b6cb146b6e07560ffbc3b544e2b179ddae04798e
ridwancodexr <ridwancodexr@gmail.com>
2026-01-16 18:27:32 +0600

    Server Create

--- Practice/p-5/index.js
+++ Practice/p-5/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const multer = require("multer");
 const app = express();
-const PORT = 4500;
+const PORT = 5500;
 
 app.get("/home", (req, res) => {
-  res.status(200).send("Welcome To The Home Page");
+  res.status(200).send(" Welcome To The Home Pag ");
 });
 
 app.listen(PORT, () => {
-  console.log(`The server is runing at http://localhost:${PORT}`);
+  console.log(`THE SERVER IS RUNING AT http://localhost:${PORT}`);
 });
