fb4d3cae82e5252d54a9dbedfd36773c4abb0d97
ridwancodexr <ridwancodexr@gmail.com>
2026-01-17 20:09:43 +0600

    DB IS CONNECTED

--- MongoDB/Lesson1/index.js
+++ MongoDB/Lesson1/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 2500;
 
@@ -6,6 +7,20 @@ app.get("/", (req, res) => {
   res.send(" <h1> Welcome To The Home Page </h1> ");
 });
 
-app.listen(PORT, () => {
+// DB Connections
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/testDB");
+    console.log("DB Is Connected");
+  } catch (error) {
+    console.log("DB Is Not Connected");
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+app.listen(PORT, async () => {
   console.log(`The Server Is Runing At http://localhost:${PORT}`);
+  await connectDB();
 });
