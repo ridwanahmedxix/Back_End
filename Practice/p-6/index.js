ad6ed010a52b60407aa4e555a29463b827d564ea
ridwancodexr <ridwancodexr@gmail.com>
2026-01-17 09:00:12 +0600

    Multer Setup

--- Practice/p-6/index.js
+++ Practice/p-6/index.js
@@ -3,8 +3,25 @@ const multer = require("multer");
 const app = express();
 const PORT = 2500;
 
-app.get("/registar", (req, res) => {
+//Multer Setup
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const name = Date.now() + "-" + file.originalname;
+    cb(null, name);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.get("/registar", upload.single("name"), (req, res) => {
   res.status(200).sendFile(__dirname + "/index.html");
+});
+app.post("/registar", (req, res) => {
+  res.status(200).send("File Is Uploaded");
 });
 
 app.get("/home", (req, res) => {
