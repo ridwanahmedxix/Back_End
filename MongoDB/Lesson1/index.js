a9be7e70ed9d7ececcfc706cb6c015b5377d1316
ridwancodexr <ridwancodexr@gmail.com>
2026-01-17 19:42:45 +0600

    Server Setup done

--- MongoDB/Lesson1/index.js
+++ MongoDB/Lesson1/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 2500;
 
+app.get("/", (req, res) => {
+  res.send(" <h1> Welcome To The Home Page </h1> ");
+});
+
 app.listen(PORT, () => {
   console.log(`The Server Is Runing At http://localhost:${PORT}`);
 });
