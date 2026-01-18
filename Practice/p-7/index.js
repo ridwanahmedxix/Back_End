97a0359be86cbd22e633e8858b7001f0d65761d5
ridwancodexr <ridwancodexr@gmail.com>
2026-01-18 07:29:28 +0600

    home route create

--- Practice/p-7/index.js
+++ Practice/p-7/index.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 3500;
 
+app.get("/", (req, res) => {
+  res.send(" <h1>  Welcome To The Home Page </h1> ");
+});
+
 app.listen(PORT, () => {
   console.log(`THE SERVER IS RUNING AT http://localhost:${PORT}`);
 });
