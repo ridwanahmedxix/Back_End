db8a850a84b8436621b5f0d42fa2694f53930ad9
ridwancodexr <ridwancodexr@gmail.com>
2026-01-18 15:45:44 +0600

    revision - 3

--- Practice/p-7/index.js
+++ Practice/p-7/index.js
@@ -3,6 +3,24 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 3500;
 
-app.listen(PORT, () => {
+// DB CONNECTIONS
+
+const connecDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/testDB");
+    console.log("DB ID CONNECTED");
+  } catch (error) {
+    console.log("DB ID NOT CONNECTED");
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("  welcome to the home page ");
+});
+
+app.listen(PORT, async () => {
   console.log(`The Server Is Runing At http://localhost:${PORT}`);
+  await connecDB();
 });
