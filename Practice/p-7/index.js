00d4b10d974396bc90dce95d93977cdf7c5e3520
ridwancodexr <ridwancodexr@gmail.com>
2026-01-19 07:53:02 +0600

    Home Page Create

--- Practice/p-7/index.js
+++ Practice/p-7/index.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 2700;
 
+app.get("/", (req, res) => {
+  res.status(200).sendFile();
+});
+
 app.listen(PORT, () => {
   console.log(`THE SERVER IS RUNING AT http://localhost:${PORT}`);
 });
