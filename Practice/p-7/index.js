138589f6b7ad62831d60d1fca5785a2ce8050f0c
ridwancodexr <ridwancodexr@gmail.com>
2026-01-19 14:10:23 +0600

    DB CONNECTIONS

--- Practice/p-7/index.js
+++ Practice/p-7/index.js
@@ -3,6 +3,19 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 3800;
 
+// DB CONNECTIONS
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/testDB");
+    console.log("DB IS CONNECTED");
+  } catch (error) {
+    console.log("DB IS NOT CONNECTED");
+    console.log(error);
+    process.exit(1);
+  }
+};
+
 app.get("/", (req, res) => {
   res.status(200).sendFile(__dirname + "/index.html");
 });
