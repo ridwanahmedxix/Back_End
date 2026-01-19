edd55050b4ae001f46d8269473e1e885ad03e308
ridwancodexr <ridwancodexr@gmail.com>
2026-01-19 08:14:57 +0600

    revision done

--- Practice/p-7/index.js
+++ Practice/p-7/index.js
@@ -3,10 +3,39 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 2700;
 
-app.get("/", (req, res) => {
-  res.status(200).sendFile();
+// Create Products Schema
+
+const productsSchema = new mongoose.Schema({
+  title: String,
+  price: Number,
+  description: String,
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-app.listen(PORT, () => {
+// Create Products Model
+const product = mongoose.model("Products", productsSchema);
+
+// DB CONNECTIONS
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/testDB");
+    console.log("DB IS CONNECTED");
+  } catch (error) {
+    console.log("DB IS NOT CONNECTED");
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+app.get("/", (req, res) => {
+  res.status(200).sendFile(__dirname + "/index.html");
+});
+
+app.listen(PORT, async () => {
   console.log(`THE SERVER IS RUNING AT http://localhost:${PORT}`);
+  await connectDB();
 });
