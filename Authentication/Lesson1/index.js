5294fc76462fb831c77e5ef34ffba47343cc9f8e
ridwancodexr <ridwancodexr@gmail.com>
2026-01-21 08:48:09 +0600

    some update

--- Authentication/Lesson1/index.js
+++ Authentication/Lesson1/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const PORT = 3600;
 
-app.listen(PORT, () => {
-  console.log(`THE SERVER IS RUNING AT http://localhost:${PORT}`);
-});
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
