ca8b2f89e90d6bfeee1b23881faedc6ebb464550
ridwancodexr <ridwancodexr@gmail.com>
2026-01-22 19:59:59 +0600

    html file create

--- Authentication/Lesson1/index.js
+++ Authentication/Lesson1/index.js
@@ -8,6 +8,12 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Home Route
+
+app.get("/", (req, res) => {
+  res.send(" <h1> Welcome To The Home Page </h1> ");
+});
+
 app.listen(process.env.PORT || 2900, () => {
   console.log(`THE SERVER IS RUNING AT http://localhost:${PORT}`);
 });
