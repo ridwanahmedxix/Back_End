bacdd3ae51db6ed0af607628595596b6834cffed
ridwancodexr <ridwancodexr@gmail.com>
2026-01-22 20:03:18 +0600

    PORT some update

--- Authentication/Lesson1/index.js
+++ Authentication/Lesson1/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const PORT = process.env.PORT || 2900;
 const app = express();
 
 app.use(cors());
@@ -11,9 +12,9 @@ app.use(express.json());
 // Home Route
 
 app.get("/", (req, res) => {
-  res.send(" <h1> Welcome To The Home Page </h1> ");
+  res.sendFile(__dirname + "/./views/index.html");
 });
 
-app.listen(process.env.PORT || 2900, () => {
+app.listen(PORT, () => {
   console.log(`THE SERVER IS RUNING AT http://localhost:${PORT}`);
 });
