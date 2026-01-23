da6c189a5f1ab85c1bb3ee4dd6b0d4666d59ae67
ridwancodexr <ridwancodexr@gmail.com>
2026-01-23 09:33:06 +0600

    database matching authentication

--- Authentication/Lesson1/index.js
+++ Authentication/Lesson1/index.js
@@ -28,10 +28,14 @@ app.get("/", (req, res) => {
 
 // Registar Route
 
-app.post("/registar", (req, res) => {
+app.post("/registar", async (req, res) => {
   try {
     const newUser = new User(req.body);
-  } catch (error) {}
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 });
 // Login Route
 
