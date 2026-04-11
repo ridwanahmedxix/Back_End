a281c90f6663dfe26da45ea874c133d2b8151edf
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-11 09:45:11 +0600

    node js bangla tutorial 4 : Module | fs module - managing files

---
+++ LearnNodeJS/L2- fsModule/index.js
@@ -0,0 +1,59 @@
+// *   --- Part 1 - writeFile / Create  -------
+
+// const fs = require("fs");
+
+// fs.writeFile("Demo1.txt", "Welcome To The Demo 1 Page ", (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successful");
+//   }
+// });
+
+// * ----- Part 2 - appendFile  ---------
+
+// const fs = require("fs");
+
+// fs.appendFile("Demo1.txt", " Some update Demo1.txt file  ", (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successful");
+//   }
+// });
+
+// * ----- Part 3 - readFile  ---------
+
+// const fs = require("fs");
+
+// fs.readFile("Demo1.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+// * ----- Part 4 - rename  ---------
+
+// const fs = require("fs");
+
+// fs.rename("Demo2.txt", (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successful");
+//   }
+// });
+
+// * ----- Part 4 - unlink / Delete   ---------
+
+// const fs = require("fs");
+
+// fs.unlink("Many.txt", (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successful");
+//   }
+// });
