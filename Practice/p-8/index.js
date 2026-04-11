e9c3a451d7f1d69c57c8dab2b062165c557799fd
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-11 09:13:18 +0600

    lesson 1 code practice

---
+++ Practice/p-8/index.js
@@ -0,0 +1,10 @@
+// const sellOneInformation = require("./products");
+const { getProduct, getPrice } = require("./products");
+
+console.log(getProduct());
+console.log(getPrice);
+
+// console.log(sellOneInformation.getProduct());
+// console.log(sellOneInformation.getPrice);
+// console.log(sellOneInformation.getUser());
+// console.log(sellOneInformation.getLocation());
---
+++ Practice/p-8/products.js
@@ -0,0 +1,25 @@
+const getProduct = () => {
+  return "Mackbook M2 ";
+};
+
+const getPrice = 2500;
+
+const getUser = () => {
+  return "Ridwan Ahmed ";
+};
+
+const getLocation = () => {
+  return "Gulshan Dhaka ";
+};
+
+module.exports = {
+  getProduct,
+  getPrice,
+  getUser,
+  getLocation,
+};
+
+// exports.getProduct = getProduct;
+// exports.getPrice = getPrice;
+// exports.getUser = getUser;
+// exports.getLocation = getLocation;
