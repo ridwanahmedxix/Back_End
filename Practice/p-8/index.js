da8b5b6817d9a10ce7e9df34a875c8cad74b4f66
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-12 09:14:53 +0600

    Lessson 1 revision

---
+++ Practice/p-8/index.js
@@ -0,0 +1,6 @@
+const studentOne = require("./products");
+
+console.log(studentOne.getName());
+console.log(studentOne.getAge);
+console.log(studentOne.getEmail());
+console.log(studentOne.getAddress());
---
+++ Practice/p-8/products.js
@@ -0,0 +1,20 @@
+const getName = () => {
+  return "Ridwan Ahmed";
+};
+
+const getAge = 19;
+
+const getEmail = () => {
+  return "ridwan@gmail.com";
+};
+
+const getAddress = () => {
+  return "Rangpure , Bangladesh";
+};
+
+module.exports = {
+  getName,
+  getAge,
+  getEmail,
+  getAddress,
+};
