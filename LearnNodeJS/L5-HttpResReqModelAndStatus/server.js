065feb5c96ec11505f4839ced696672c3c627f11
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-12 14:15:09 +0600

    node js bangla tutorial 7 : http res, req model and status code

--- LearnNodeJS/L5-HttpResReqModelAndStatus/server.js
+++ LearnNodeJS/L5-HttpResReqModelAndStatus/server.js
@@ -1,9 +1,31 @@
+// ---------- PART - 1 ------------------
+
+// const http = require("http");
+// const PORT = 1900;
+// const localHost = "127.0.0.1";
+
+// const myServer = http.createServer((req, res) => {
+//   res.writeHead(202, { "Content-Type": "text/plain" });
+//   res.write("Hello world");
+//   res.end();
+// });
+
+// myServer.listen(PORT, localHost, () => {
+//   console.log(`The Server Is Runing At http://${localHost}:${PORT}`);
+// });
+
+// ---------- PART - 2 ------------------
+// "Content-Type": "text/html"
+
 const http = require("http");
 const PORT = 1900;
 const localHost = "127.0.0.1";
 
 const myServer = http.createServer((req, res) => {
-  res.end(" <h1> Hello World </h1>  ");
+  res.writeHead(202, { "Content-Type": "text/html" });
+  res.write(" <h1> Hello world </h1>  ");
+  res.write(" <h1> Hello Everyone </h1>  ");
+  res.end();
 });
 
 myServer.listen(PORT, localHost, () => {
