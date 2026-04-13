82e0d684179d762f85b34edf3581c68f7d390aaa
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-13 08:31:13 +0600

    file basic setup and server create

---
+++ Projects/NodeJs-MiniProjects/p1-SimpleStudentInfoServer/server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const PORT = 2900;
+const localHost = "127.0.0.1";
+
+const myServer = http.createServer((req, res) => {
+  res.end("Hello world");
+});
+
+myServer.listen(PORT, localHost, () => {
+  console.log(`THE SERVER IS RUNING AT http://${localHost}:${PORT}`);
+});
