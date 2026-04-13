d35f89299bc2704d9ad02d5a3aa1043afeb0061f
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-13 11:08:35 +0600

    Simple Student Info Server

--- Projects/NodeJs-MiniProjects/p1-SimpleStudentInfoServer/server.js
+++ Projects/NodeJs-MiniProjects/p1-SimpleStudentInfoServer/server.js
@@ -11,7 +11,7 @@ const myServer = http.createServer((req, res) => {
     <h1>Welcome To My Server</h1>
     <br />
     <a href="/name"> Name </a> <br />
-    <a href="/age"> Age </a>
+    <a href="/age"> Age </a> <br/>
     <a href="/email"> Email </a> 
     `);
   } else if (req.url === "/name") {
@@ -23,6 +23,11 @@ const myServer = http.createServer((req, res) => {
   } else if (req.url === "/age") {
     res.end(`
         <h1> Age : ${student.getAge()} </h1>
+        <a href="/"> Go Back </a>
+    `);
+  } else if (req.url === "/email") {
+    res.end(`
+         <h1> Age : ${student.getEmail()} </h1>
         <a href="/"> Go Back </a>
         `);
   }
