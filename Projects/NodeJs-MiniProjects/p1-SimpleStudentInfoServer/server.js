1e6d11c4acc9cebe506f724351fae28a702b0f36
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-13 10:41:10 +0600

    home route create

--- Projects/NodeJs-MiniProjects/p1-SimpleStudentInfoServer/server.js
+++ Projects/NodeJs-MiniProjects/p1-SimpleStudentInfoServer/server.js
@@ -4,6 +4,15 @@ const localHost = "127.0.0.1";
 
 const myServer = http.createServer((req, res) => {
   res.writeHead(202, { "Content-Type": "text/htl" });
+  if (req.url === "/") {
+    res.end(`   
+    <h1>Welcome To My Server</h1>
+    <br />
+    <a href="/name"> Name </a> <br />
+    <a href="/age"> Age </a>
+    <a href="/email"> Email </a> 
+    `);
+  }
 });
 
 myServer.listen(PORT, localHost, () => {
