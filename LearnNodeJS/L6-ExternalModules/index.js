bdf673119adb98a470cf58f688ddb41679d59584
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-14 09:16:45 +0600

    gitignore add

--- LearnNodeJS/L6-ExternalModules/index.js
+++ LearnNodeJS/L6-ExternalModules/index.js
@@ -1,4 +1,4 @@
 const getRandomFruitsName = require("random-fruits-name");
 
 console.log(getRandomFruitsName("en"));
-// console.log(getRandomFruitsName('en'));
+// console.log(getRandomFruitsName("bn"));
