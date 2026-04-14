61a23c36330b5505310e777640aebec698d7f3b8
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-14 09:22:19 +0600

    node js bangla tutorial 8 : Module | external modules | npm crash course

--- LearnNodeJS/L6-ExternalModules/index.js
+++ LearnNodeJS/L6-ExternalModules/index.js
@@ -2,3 +2,4 @@ const getRandomFruitsName = require("random-fruits-name");
 
 console.log(getRandomFruitsName("en"));
 // console.log(getRandomFruitsName("bn"));
+// console.log(getRandomFruitsName('ja')) // りんご
