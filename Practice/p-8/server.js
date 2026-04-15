2895c0e8ff94c613af982958edd999c60804f0f4
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-15 08:04:47 +0600

    first commit

--- Practice/p-8/server.js
+++ Practice/p-8/server.js
@@ -1,14 +1,14 @@
-const http = require("http");
-const PORT = 2900;
-const localHost = "127.0.0.1";
+// const http = require("http");
+// const PORT = 2900;
+// const localHost = "127.0.0.1";
 
-const myServer = http.createServer((req, res) => {
-  res.writeHead(202, { "Content-Type": "text/plain" });
-  res.write("Hello world");
+// const myServer = http.createServer((req, res) => {
+//   res.writeHead(202, { "Content-Type": "text/plain" });
+//   res.write("Hello world");
 
-  res.end();
-});
+//   res.end();
+// });
 
-myServer.listen(PORT, localHost, () => {
-  console.log(`THE SERVER IS RUNING AT http://${localHost}:${PORT}`);
-});
+// myServer.listen(PORT, localHost, () => {
+//   console.log(`THE SERVER IS RUNING AT http://${localHost}:${PORT}`);
+// });
