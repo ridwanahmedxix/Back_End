715c45b0b67a03cc438dbb570d149afa4a89b624
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-15 08:46:56 +0600

    home route create

--- LearnNodeJS/L7-httpRoutingAndNodemonPackage/index.js
+++ LearnNodeJS/L7-httpRoutingAndNodemonPackage/index.js
@@ -1,9 +1,16 @@
 const http = require("http");
+const fs = require("fs");
 const PORT = 3100;
 const localHost = "127.0.0.1";
 
 const myServer = http.createServer((req, res) => {
-  res.end("Welcome to my server");
+  if (req.url === "/") {
+    fs.readFile("index.html", (err, data) => {
+      res.writeHead(202, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  }
 });
 
 myServer.listen(PORT, localHost, () => {
