1ddee24ab39d8462fd8f8c995be9315ad20ce108
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-15 08:58:32 +0600

    node routing create

--- LearnNodeJS/L7-httpRoutingAndNodemonPackage/index.js
+++ LearnNodeJS/L7-httpRoutingAndNodemonPackage/index.js
@@ -10,6 +10,24 @@ const myServer = http.createServer((req, res) => {
       res.write(data);
       res.end();
     });
+  } else if (req.url === "/about") {
+    fs.readFile("about.html", (err, data) => {
+      res.writeHead(202, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url === "/contact") {
+    fs.readFile("contact.html", (err, data) => {
+      res.writeHead(202, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else {
+    fs.readFile("error.html", (err, data) => {
+      res.writeHead(500, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
   }
 });
 
