943df395b65ef760a64991e3b3c4fe2fc76851a4
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-16 19:11:23 +0600

    server and port create

--- Practice/PracticeNode/server.js
+++ Practice/PracticeNode/server.js
@@ -1,2 +1,12 @@
 const student = require("./index");
 const http = require("http");
+const PORT = 3200;
+const localHost = "127.0.0.1";
+
+const myServer = http.createServer((req, res) => {
+  res.end("Hello World");
+});
+
+myServer.listen(PORT, localHost, () => {
+  console.log(`THE SERVER IS RUNING AT http://${localHost}:${PORT}`);
+});
