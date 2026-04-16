ebd6ceed182f77c0df64fd2d0a2b218351e54613
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-16 19:14:21 +0600

    status code set

--- Practice/PracticeNode/server.js
+++ Practice/PracticeNode/server.js
@@ -4,7 +4,7 @@ const PORT = 3200;
 const localHost = "127.0.0.1";
 
 const myServer = http.createServer((req, res) => {
-  res.end("Hello World ");
+  res.writeHead(202, { "Content-Type": "text/html" });
 });
 
 myServer.listen(PORT, localHost, () => {
