99759d5f056c6991ecb961e4a0863aa181b48d9f
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-17 19:16:29 +0600

    node js routing create

--- LearnNodeJS/A-NodePractice/practice-1/index.js
+++ LearnNodeJS/A-NodePractice/practice-1/index.js
@@ -18,6 +18,12 @@ const handleReadFile = (fileName, statusCode, req, res) => {
 const myServer = http.createServer((req, res) => {
   if (req.url === "/") {
     handleReadFile("./views/index.html", 200, req, res);
+  } else if (req.url === "/about") {
+    handleReadFile("./views/about.html", 200, req, res);
+  } else if (req.url === "/contact") {
+    handleReadFile("./views/contact.html", 200, req, res);
+  } else {
+    handleReadFile("./views/error.html", 200, req, res);
   }
 });
 
