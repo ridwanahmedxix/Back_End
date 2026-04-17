c96cbd66fa98c9b1d6ecb4a91e0f04976dea45bb
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-17 19:12:57 +0600

    path bug fixed

--- LearnNodeJS/A-NodePractice/practice-1/index.js
+++ LearnNodeJS/A-NodePractice/practice-1/index.js
@@ -3,19 +3,21 @@ const http = require("http");
 const PORT = 2300;
 const localHost = "127.0.0.1";
 
-const handleReadFile = () => {
-  fs.readFile("index.html", "utf-8", (err, data) => {
+const handleReadFile = (fileName, statusCode, req, res) => {
+  fs.readFile(fileName, "utf-8", (err, data) => {
     if (err) {
       console.log(err);
     } else {
       res.writeHead(202, { "Content-Type": "text/html" });
       res.write(data);
+      res.end();
     }
   });
 };
 
 const myServer = http.createServer((req, res) => {
   if (req.url === "/") {
+    handleReadFile("./views/index.html", 200, req, res);
   }
 });
 
