b13f63a28bbcf57f955f942f57d16e086ce4d440
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-17 14:26:48 +0600

    all update

--- LearnNodeJS/A-NodePractice/practice-1/index.js
+++ LearnNodeJS/A-NodePractice/practice-1/index.js
@@ -1,11 +1,18 @@
 const fs = require("fs");
 const http = require("http");
-const PORT = 3300;
+const PORT = 2300;
 const localHost = "127.0.0.1";
 
 const myServer = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.writeHead(202, { "Content-Type": "text/html" });
+    fs.readFile("index.html", "utf-8", (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.writeHead(202, { "Content-Type": "text/html" });
+        res.write(data);
+      }
+    });
   }
 });
 
