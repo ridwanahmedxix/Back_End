a1947f25a52e1261900e1af6a55965a28b9d5091
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-18 14:56:00 +0600

    handleReadFile function create

--- LearnNodeJS/A-NodePractice/practice-2/index.js
+++ LearnNodeJS/A-NodePractice/practice-2/index.js
@@ -3,6 +3,18 @@ const http = require("http");
 const PORT = 2900;
 const localHost = "127.0.0.1";
 
+const handleReadFile = (fileName, statusCode, req, res) => {
+  fs.readFile(fileName, "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.writeHead(202, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    }
+  });
+};
+
 const myServer = http.createServer((req, res) => {
   res.end("Helllo World");
 });
