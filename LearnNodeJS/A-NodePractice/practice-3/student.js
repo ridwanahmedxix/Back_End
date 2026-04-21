c2af0d7a7542a81aa268b00601d8e3db57cda1a9
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-21 07:56:08 +0600

    first commit

---
+++ LearnNodeJS/A-NodePractice/practice-3/student.js
@@ -0,0 +1,13 @@
+const getName = () => {
+  return "Ridwan Ahmed";
+};
+
+const getAge = () => {
+  return "My Age 19";
+};
+
+const getEmail = () => {
+  return "ridwan@gmail.com";
+};
+
+module.exports = { getName, getAge, getEmail };
