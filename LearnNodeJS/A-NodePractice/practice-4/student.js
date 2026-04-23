2bc42a3a4ee6f9661ed319294f504432e5278d5f
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-23 18:59:43 +0600

    student info create

---
+++ LearnNodeJS/A-NodePractice/practice-4/student.js
@@ -0,0 +1,15 @@
+const getName = () => {
+  return "Nusrat Jahan Tisha";
+};
+
+const getAge = () => {
+  return " Her Age 15";
+};
+
+const getSchool = () => {
+  return " Komor vangi high school ";
+};
+
+const getClass = () => {
+  return "Class 9";
+};
